1422a55d3af2b5739079b955fac3bb2d86cf5474
code-rius <telephant@protonmail.com>
2020-11-13 23:11:05 +0200

    R calibration tweaks

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
 import numpy as np
 from recurrence_plot import RecurrencePlot as rp
 import csv
-
+import os
 csvData=[]
 
 time = np.arange(0, 180 , 0.3)
 amplitude = np.round(np.sin(time), 5)
+path = os.path.dirname(os.path.realpath(__file__))
 
-with open("dow-jones.csv") as csvfile:
+with open(path + "/assets/dow-jones.csv") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         csvData.append(float(row[4]))
         if (len(csvData)>=1000): break
 
-dow = rp(2 , 1, csvData)
-dow.draw_diagram()
+dow = rp(10 , 3, csvData, 1)
+# dow.draw_diagram()
--- src/recurrence_plot.py
+++ src/recurrence_plot.py
@@ -3,16 +3,19 @@ import matplotlib.pyplot as plot
 import numpy as np
 from scipy import signal
 
+import timeit
+
 class RecurrencePlot:
 
-    def __init__(self, D: int, d: int, data: list, target: float = 15,downSample: bool = False, downSampleTarget: int = 720):
+    def __init__(self, D: int, d: int, data: list, compareMode:int = 0, target: float = 15,downSample: bool = False, downSampleTarget: int = 720):
+        self.downSampleTarget = downSampleTarget
+        self.compareMode = compareMode
+        self.downSample = downSample
+        self.target = target
+        self.data = data
         self.D = D
         self.d = d
-        self.data = data
-        self.target = target
-        self.downSample = downSample
-        self.downSampleTarget = downSampleTarget
-        self.allowed_r_deviation = 0.0005
+        self.allowed_r_deviation = 1
         self.N = len(data)
         self.M = self.N-(D-1)*d
         self.r = self.percentage = 0
@@ -20,24 +23,33 @@ class RecurrencePlot:
         self.calibrate_r()
 
     def calibrate_r(self):
-        r_last = (max(self.data) - min(self.data))*np.sqrt(self.D)
-        percentage_last=100
+        if (self.compareMode == 0):
+            r_last = (max(self.data) - min(self.data))*np.sqrt(self.D)
+        elif (self.compareMode == 1):
+            r_last = (max(self.data) - min(self.data))
 
-        counter = 0
+        self.percentage = percentage_last = 100
+
+        # Repeat until R is calibrated
         while (abs(self.percentage-self.target) > self.allowed_r_deviation):
+            t_start = timeit.default_timer()
+
             self.r = (self.r+r_last)/(self.percentage+percentage_last)*self.target
             self.calculate_recurrences()
+
             percentage_last = self.percentage
             r_last = self.r
-            
-            print("R", counter, "=", self.r, "\tNow %", counter, "=", self.percentage)
-            counter +=1
+
+            t_end = timeit.default_timer()
+
+            print("R=", self.r, "\tNow %=", self.percentage,
+                  "\tTime taken: ", t_end - t_start)
 
     def calculate_recurrences(self):
         similarities = []
         sygnalStates = []
 
-        # Generate data pairs, tripplets, quadruplets ... depending on D
+        # Generate data pairs, tripplets, quadruplets ... D - uplets
         for i in range(0, self.M):
             state = []
 
@@ -47,16 +59,32 @@ class RecurrencePlot:
             sygnalStates.append(state)
 
         # Generate data similarities array
-        for i in range(0, self.M):
-            for j in range(i, self.M):
-                #TODO: Parametrized and moved to a function
-                vectorDifference = np.subtract(
-                    sygnalStates[i], sygnalStates[j])
+        if (self.compareMode == 0):
+            # Calculate using euclidian norm
+            for i in range(0, self.M):
+                for j in range(i, self.M):
+                    vectorDifference = []
+                    
+                    for k in range(0, self.D):
+                        vectorDifference.append(
+                            sygnalStates[i][k] - sygnalStates[j][k])
 
-                vectorDistance = np.linalg.norm(vectorDifference)
+                    vectorDistance = np.linalg.norm(vectorDifference)
 
-                if (vectorDistance <= self.r):
-                    similarities.append([i, j])
+                    if (vectorDistance <= self.r):
+                        similarities.append([i, j])
+
+        elif (self.compareMode == 1):
+            # Calculate using the maximum value
+            for i in range(0, self.M):
+                for j in range(i, self.M):
+                    vectorDifference = []
+
+                    for k in range(0, self.D):
+                        vectorDifference.append(sygnalStates[i][k] - sygnalStates[j][k])
+
+                    if (abs(max(vectorDifference)) <= self.r):
+                        similarities.append([i, j])
 
         self.similarities = similarities
         self.get_pixel_percentage()
