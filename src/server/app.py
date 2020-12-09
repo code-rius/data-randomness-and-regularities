b214d1d3f30539d29e44e3c204d18cd832c27689
Audrius Baranauskas <telephant@protonmail.com>
2020-12-09 18:58:07 +0200

    Updated project structure

---
+++ src/server/app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from recurrence_plot import RecurrencePlot as rp
+import csv
+import os
+import json
+csvData=[]
+
+time = np.arange(0, 180 , 0.3)
+amplitude = np.round(np.sin(time), 5)
+path = os.path.dirname(os.path.realpath(__file__))
+
+with open(path + "/assets/DJI.csv") as csvfile:
+    csvfile.readline()   # skip the first line of headers
+    reader = csv.reader(csvfile)
+    for row in reader:
+        csvData.append(float(row[4]))
+        # if (len(csvData)>=1150): break
+
+dow = rp(3 , 1, csvData, 1)
+
+dow.draw_diagram()
